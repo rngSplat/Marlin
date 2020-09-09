05e667caeabfd6b9f464771a02305e2449390fa9
Dan Tabor <dtabor1@gmail.com>
2020-09-09 19:01:21 -0400

    Z current to 1240

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2258,7 +2258,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       1200
+    #define Z_CURRENT       1240
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
