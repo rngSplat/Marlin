647bd68b2df68f1af1fbf7bd8b47ab9a9ba83feb
Dan Tabor <dtabor1@gmail.com>
2020-09-16 19:47:15 -0400

    XY max accel up to 2000 from 1000

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -772,7 +772,7 @@
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 1000, 1000, 500, 5000 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 2000, 2000, 500, 5000 } // ...or, set your own edit limits
 #endif
 
 /**
