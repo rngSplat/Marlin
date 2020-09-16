c46493f62c1efdb8adc7eb8c3fb8a4c350b2642d
Dan Tabor <dtabor1@gmail.com>
2020-09-16 19:46:42 -0400

    Z Max accel up to 500 from 200

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -768,11 +768,11 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 200, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 500, 5000 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
-  #define MAX_ACCEL_EDIT_VALUES       { 1000, 1000, 200, 5000 } // ...or, set your own edit limits
+  #define MAX_ACCEL_EDIT_VALUES       { 1000, 1000, 500, 5000 } // ...or, set your own edit limits
 #endif
 
 /**
