b61168635d7e8d6cae367e5ef1f5c9ae22cc1089
Dan Tabor <dtabor1@gmail.com>
2020-09-17 15:44:50 -0400

    probing margin up to 30, xy probe speed up to 6000

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1001,10 +1001,10 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 30
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED 5000
+#define XY_PROBE_SPEED 6000
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
