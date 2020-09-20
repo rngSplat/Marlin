ab92348f469b8d34dfe7ec66a0be19f838e17d22
Dan Tabor <dtabor1@gmail.com>
2020-09-19 22:00:13 -0400

    extend PSU control timeout and powerup delay

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -332,8 +332,8 @@
 #if ENABLED(PSU_CONTROL)
   #define PSU_ACTIVE_STATE HIGH      // Set 'LOW' for ATX, 'HIGH' for X-Box
 
-  //#define PSU_DEFAULT_OFF         // Keep power off until enabled directly with M80
-  #define PSU_POWERUP_DELAY 250   // (ms) Delay for the PSU to warm up to full power
+  #define PSU_DEFAULT_OFF         // Keep power off until enabled directly with M80
+  #define PSU_POWERUP_DELAY 2500   // (ms) Delay for the PSU to warm up to full power
 
   #define AUTO_POWER_CONTROL      // Enable automatic control of the PS_ON pin
   #if ENABLED(AUTO_POWER_CONTROL)
@@ -343,7 +343,7 @@
     #define AUTO_POWER_CHAMBER_FAN
     #define AUTO_POWER_E_TEMP        50 // (°C) Turn on PSU over this temperature
     //#define AUTO_POWER_CHAMBER_TEMP  30 // (°C) Turn on PSU over this temperature
-    #define POWER_TIMEOUT 30
+    #define POWER_TIMEOUT 1800
   #endif
 #endif
 
