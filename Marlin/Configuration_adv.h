c114a985ad0cb350448761060747d90c7c01f47e
Dan Tabor <dtabor1@gmail.com>
2020-09-28 11:26:39 -0400

    disable faster gcode parser

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3139,7 +3139,7 @@
 /**
  * Spend 28 bytes of SRAM to optimize the GCode parser
  */
-#define FASTER_GCODE_PARSER
+//#define FASTER_GCODE_PARSER
 
 #if ENABLED(FASTER_GCODE_PARSER)
   //#define GCODE_QUOTED_STRINGS  // Support for quoted string parameters
