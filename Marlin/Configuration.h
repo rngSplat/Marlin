8d19f2a087e1f08696cf33a6c0df08160e56c9a2
Dan Tabor <dtabor1@gmail.com>
2020-09-30 11:18:00 -0400

    changing steps per unit for E axis to accomodate 1.8degree stepper

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -750,7 +750,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 811.30 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 415 }
 
 /**
  * Default Max Feed Rate (mm/s)
