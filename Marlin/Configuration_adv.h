79e19c9ce168e1180a375b3cc30bd2e248e4a7f8
Dan Tabor <dtabor1@gmail.com>
2020-09-30 12:51:48 -0400

    updated E0 current for 1.8degreee stepper

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2290,7 +2290,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      1100
+    #define E0_CURRENT      500
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
